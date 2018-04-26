1637c72839d6f33d1e6fa6f136e55827a12d049f
Gabriel Gripp <grippado@gmail.com>
2018-04-26 10:22:46 -0300

    remove methods to class

--- src/components/Monster.vue
+++ src/components/Monster.vue
@@ -1,65 +1,5 @@
 <template>
-  <main class="main-content">
-    <section v-if="errors && errors.length" class="errors-section">
-        <div class="container">
-          <ul>
-          <li v-for="error of errors">
-            {{error.message}}
-          </li>
-        </ul>
-        </div>
-    </section>  
-    <section v-if="loading" class="pokedex-loader">
-      <div class="container">
-        <h1 class="page-title">Loading...</h1>
-      </div>
-    </section>
-    <section v-if="!loading" class="pokemon-details-section">
-        <div class="container">
-          <h1 class="main-title">{{ monster.id }}. <span class="monster-name">{{ monster.name }}</span></h1>
-          <div class="panel">
-              <div class="panel-body">
-                  <div class="row">
-                      <div class="col">
-                          <img class="monster-image" v-if="monster.sprites" :src="monster.sprites.front_default" alt="">
-                      </div>
-                      <div class="col">
-                          <table class="stats-table">
-                              <tr>
-                                  <th 
-                                    v-for="row in monster.stats">
-                                      {{ row.stat.name }}
-                                  </th>
-                              </tr>
-                              <tr>
-                                <th 
-                                  v-for="row in monster.stats">
-                                    {{ row.base_stat }}
-                                  </th>
-                              </tr>
-                          </table>
-                          <ul class="monster-types-list">
-                            <li 
-                              v-for="monsterType in monsterTypes" 
-                              :key="monsterType.slot"
-                              class="monster-types-list__item" 
-                              :class="'monster-types-list__item--' + monsterType.name" 
-                              v-bind:style="{ borderColor: monsterType.color, backgroundColor: monsterType.color }">
-                                <span>{{ monsterType.name }}</span>
-                            </li>
-                          </ul>
-                      </div>
-                  </div>
-              </div>  <!-- .panel-bpdy -->
-              <div class="panel-footer">
-                  <router-link :to="'/pokemon/' + prevId + '/'" class="back-link">&larr; Previous Pokemon</router-link>
-                  <router-link to="/" class="back-link">Back to Pokemon List</router-link>
-                  <router-link :to="'/pokemon/' + nextId + '/'" class="back-link">Next Pokemon &rarr;</router-link>
-              </div>
-          </div>
-        </div>
-    </section>
-  </main>
+
 </template>
 
 <script>
@@ -73,7 +13,7 @@ export default {
       monster: null,
       errors: [],
 
-      // borrowed from https://github.com/nolanlawson/pokedex.org/blob/master/src/js/shared/monster/typesToColors.js
+      // catched from https://github.com/nolanlawson/pokedex.org/blob/master/src/js/shared/monster/typesToColors.js
       colors: {
         normal: '#A8A878',
         fire: '#F08030',
@@ -98,33 +38,8 @@ export default {
   },
   mounted() {
     let vm = this;
-    // console.log("Monster mounted");
   },
   methods: {
-    getMonster(){
-      let vm = this;
-      this.errors = this.monster = null;
-      vm.loading = true
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${vm.$route.params.id}`)
-        .then(response => {
-          // JSON responses are automatically parsed.
-          // console.log(response);
-          //   this.totalResults = response.data.count;
-          vm.monster = response.data;
-          vm.loading = false;
-        })
-        .catch(e => {
-          vm.errors.push(e);
-          vm.loading = false;
-        });
-    },
-    getColorFromType(type){
-      console.log('')
-      console.log('Getting color:')
-      console.log(type)
-      console.log('')
-      return this.colors[type];
-    }
   },
   watch: {
     '$route': 'getMonster'
@@ -135,26 +50,6 @@ export default {
     vm.getMonster();
   },
   computed: {
-    id(){
-      return this.$route.params.id;
-    },
-    nextId(){
-      return (parseInt(this.id) + 1).toString();
-    },
-    prevId(){
-      return (parseInt(this.id) - 1).toString();
-    },
-    monsterTypes(){
-      let vm = this;
-      return vm.monster.types.map(type => {
-        // return type;
-        return {
-          name: type.type.name,
-          color: vm.getColorFromType(type.type.name),
-          slot: type.slot
-        }
-      })
-    }
   }
 };
 </script>
--- src/components/MonsterList.vue
+++ src/components/MonsterList.vue
@@ -1,35 +1,8 @@
 <template>
-  <main class="main-content">
-    <section v-if="errors && errors.length" class="errors-section">
-        <ul>
-          <li v-for="error of errors">
-            {{error.message}}
-          </li>
-        </ul>
-    </section>  
-    <section v-if="loading" class="pokedex-loader">Loading...</section>
-    <section v-if="!loading" class="pokemon-list-section">
-      <div class="panel">
-        <div class="panel-heading">{{ msg }}</div>
-        <div class="panel-body">
-          <ul v-if="processedPokedmon && processedPokedmon.length">
-            <li v-for="(monster, id) in processedPokedmon">
-              <router-link :to="'/pokemon/' + monster.id + '/'">
-                <strong>{{ monster.id }}. {{monster.formattedName}}</strong>
-              </router-link>
-            </li>
-          </ul>
-
-        </div>  
-      </div>
-    </section>
-  </main>
 </template>
-
 <script>
 
 import axios from 'axios'
-
 export default {
   name: 'MonsterList',
   data () {
@@ -41,14 +14,10 @@ export default {
       msg: 'List of Pokemon'
     }
   },
-  mounted () {
-    console.log('MonsterList mounted')
-  },
   created () {
     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     .then(response => {
       // JSON responses are automatically parsed.
-      console.log(response)
       this.totalResults = response.data.count
       this.pokemon = response.data.results
       this.loading = false;
@@ -57,30 +26,8 @@ export default {
       this.errors.push(e)
       this.loading = false;
     })
-
-    // async / await version (created() becomes async created())
-    //
-    // try {
-    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    //   this.posts = response.data
-    // } catch (e) {
-    //   this.errors.push(e)
-    // }
   },
   computed: {
-    processedPokedmon(){
-      return this.pokemon.map(pokemon => {
-        let monster = {
-          name: pokemon.name
-        };
-        let monsterUrlPathArray = pokemon['url'].split( '/' );
-        // uppercase first letter of pokemon name. they come over lowercased
-        monster['formattedName'] = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-        // extract ID from URL for use in the router-link URL
-        monster['id'] = monsterUrlPathArray[monsterUrlPathArray.length - 2];
-        return monster;
-      })
-    }
   }
 }
 </script>
